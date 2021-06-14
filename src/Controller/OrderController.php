9094b5eb9eeebd870739710f07d126545a6d333d
guillaumebeysson <guillaume.beysson@gmail.com>
2021-06-14 10:53:41 +0200

    lundi 11h

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
 use App\Session\Cart;
 use App\Form\OrderType;
+use App\Entity\OrderDetails;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -48,7 +51,7 @@ class OrderController extends AbstractController
     /**
      * @Route("/order/recap", name="order_recap")
      */
-    public function recap(Request $request, Cart $cart, ProductRepository $repo): Response
+    public function recap(Request $request, Cart $cart, ProductRepository $repo, EntityManagerInterface $em): Response
     {
         //Je crée une variable 'fullInfoProduct'
         $fullInfoProduct = [];
@@ -90,7 +93,48 @@ class OrderController extends AbstractController
             $addressDelivery .= ' , ' . $delivery->getCity();
             $addressDelivery .= ' - ' . $delivery->getCountry();
 
+            $order = new Order();
+            $order->setUser($this->getUser());
+            $order->setCreatedAt($date);
 
+            $order->setDeliverer($deliverer->getName());
+            $order->setDelivererPrice($deliverer->getPrice());
+            $order->setDeliveryAddress($addressDelivery);
+            $order->setIsPaid(0);
+
+            $em->persist($order);
+
+            
+            //Je crée une variable 'fullInfoProduct'
+        $fullInfoProduct = [];
+        foreach ($cart->get() as $id => $quantity) {
+            $product = $repo->findOneById($id);
+
+            //si aucun produit n'existe, continu
+            if(!$product){
+                continue;
+            }
+
+            $fullInfoProduct[] = [ 
+                'product' => $product,
+                'quantity' => $quantity,
+            ];
+        }
+
+            
+            
+            foreach ($fullInfoProduct as $product) {
+                $orderDetails = new OrderDetails();
+                $orderDetails->setMyOrder($order);
+                $orderDetails->setProduct($product['product']->getName());
+                $orderDetails->setQuantity($product['quantity']);
+                $orderDetails->setPrice($product['product']->getPrice());
+                $orderDetails->setTotal($product['product']->getPrice() * $product['quantity']);
+
+                $em->persist($orderDetails);
+            }
+
+            $em->flush();
 
         }
 
